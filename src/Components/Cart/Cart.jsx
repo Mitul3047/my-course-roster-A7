e2a7d955787f72f2ae24da4321bfe6521972be06
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-09-15 00:28:28 +0600

    card design done

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,23 +1,25 @@
 import PropTypes from "prop-types";
 
-const Cart = ({selectCourse}) => {
+const Cart = ({ selectCourse }) => {
 
-   
+
     return (
         <div className="bg-base-100 p-6 rounded-lg">
-            <h3 className="">Credit Hour Remaining 7 hr</h3>
-                {
-                    selectCourse.map(course =>(
-                        <li key={course.id} className="list-decimal font-normal text-[#1C1B1B99]">{course.title}</li>
-                    ))
-                }
-
+            <h3 className="border-b-2 pb-4 text-[#2F80ED] text-lg font-bold">Credit Hour Remaining 7 hr</h3>
+            <h3 className="text-[#1C1B1B] text-xl font-bold pt-4 pb-5">Course Name</h3>
+            {
+                selectCourse.map(course => (
+                    <li key={course.id} className="list-decimal font-normal text-[#1C1B1B99]">{course.title}</li>
+                ))
+            }
+            <h4 className="border-t-2 p-4 font-medium text-[#1C1B1BCC]">Total Credit Hour : 13</h4>
+            <h4 className="border-t-2 p-4 font-semibold text-[#1C1B1BCC]">Total Price : 48000 USD</h4>
 
 
         </div>
     );
 };
-Cart.propTypes ={
-    selectCourse : PropTypes.array
+Cart.propTypes = {
+    selectCourse: PropTypes.array
 }
 export default Cart;
